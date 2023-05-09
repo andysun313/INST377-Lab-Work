cacc4dfcf37b0ea0ce35a6790b6804cde080e79e
andysun313 <123782745+andysun313@users.noreply.github.com>
2023-05-09 18:07:05 -0400

    Including External Library

---
+++ final_project/script.js
@@ -0,0 +1,183 @@
+/*
+  Hook this script to index.html
+  by adding `<script src="script.js">` just before your closing `</body>` tag
+*/
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+  
+  function injectHTML(list) {
+    console.log("fired injectHTML");
+    const target = document.querySelector("#restaurant_list");
+    target.innerHTML = "";
+    list.forEach((item) => {
+      const str = `<li>${item.name}</li>`;
+      target.innerHTML += str;
+    });
+  }
+  
+  function filterList(list, query) {
+    return list.filter((item) => {
+      const lowerCaseName = item.name.toLowerCase();
+      const lowerCaseQuery = query.toLowerCase();
+      return lowerCaseName.includes(lowerCaseQuery);
+    });
+  }
+  
+  function cutRestaurantList(list) {
+    console.log("fired cut list");
+    const range = [...Array(15).keys()];
+    return (newArray = range.map((item) => {
+      const index = getRandomIntInclusive(0, list.length - 1);
+      return list[index];
+    }));
+  }
+  
+  function initMap() {
+      //38.9072° N, 77.0369° W
+    const carto = L.map("map").setView([38.98, -76.93], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(carto);
+    return carto;
+  }
+  
+  function markerPlace(array,map) {
+  console.log('array for markers', array);
+  
+  map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+  
+  array.forEach((item) => {
+      console.log('markerPlace', item);
+      const {coordinates} = item.geocoded_column_1;
+      L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  })
+  }
+  
+  function initChart(target, data, labels) {  
+   const chart = new Chart(target, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: '# of Votes',
+          data: data,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+    return chart;
+  }
+
+    function processChartData(data) {
+        const dataForChart = data.reduce((col, item, idx) => {
+
+            if (!col[item.category]) {
+                col[item.category] = 1
+            }
+            else{
+                col[item.category] +=1
+            }
+    
+            return col;
+        }, {})
+    
+        const dataSet = Object.values(dataForChart);
+        const labels = Object.keys(dataForChart);
+    
+        //console.log(dataForChart);
+        return [dataSet, labels];
+    }
+
+    function updateChart(chart, newInfo) {
+        const chartData = processChartData(newInfo);
+        chart.data.labels = chartData[1];
+        chart.data.datasets.forEach((item) => data = chartData[0]);
+        chart.update();
+    }
+
+  async function mainEvent() {
+    // the async keyword means we can make API requests
+    const loadDataButton = document.querySelector("#data_load");
+    const clearDataButton = document.querySelector("#data_clear");
+    const generateListButton = document.querySelector("#generate");
+    const textField = document.querySelector("#list_selector");
+    const chart = document.querySelector('#myChart');
+  
+    const loadAnimation = document.querySelector("#data_load_animation");
+    loadAnimation.style.display = "none";
+    generateListButton.classList.add("hidden");
+  
+    const storedData = localStorage.getItem("storedData");
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0) {
+      generateListButton.classList.remove("hidden");
+    }
+  
+    let currentList = [];
+
+    const chartData = processChartData(parsedData);
+    const newChart = initChart(chart, chartData[0], chartData[1]);
+  
+    loadDataButton.addEventListener("click", async (submitEvent) => {
+      console.log("Loading Data"); // this is substituting for a "breakpoint"
+      loadAnimation.style.display = "inline-block";
+  
+      const results = await fetch(
+        "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
+      );
+  
+      const storedList = await results.json();
+      localStorage.setItem("storedData", JSON.stringify(storedList));
+  
+      parsedData = storedList;
+  
+      if (parsedData?.length > 0) {
+          generateListButton.classList.remove("hidden");
+        }
+      
+  
+      loadAnimation.style.display = "none";
+      // console.table(storedList);
+    });
+  
+    generateListButton.addEventListener("click", (event) => {
+      console.log("generate new list");
+      currentList = cutRestaurantList(parsedData);
+      console.log(currentList);
+      injectHTML(currentList);
+      updateChart(newChart, currentList);
+    });
+  
+    textField.addEventListener("input", (event) => {
+      console.log("input", event.target.value);
+      const newList = filterList(currentList, event.target.value);
+      console.log(newList);
+      injectHTML(newList);
+    });
+  
+    // TODO: add data clear button
+    //clearDataButton.addEventListener("click", (event) => {
+    //  console.log('clear browser data');
+    //  localStorage.clear();
+    //  console.log('localStorage Check', localStorage.getItem("s"))
+   // })
+  }
+  
+  document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
+  
